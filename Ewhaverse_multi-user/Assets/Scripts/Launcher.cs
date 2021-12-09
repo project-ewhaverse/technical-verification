ec9f519db636375982f8dec577b284b7389816b3
Koohi45 <haein0405@ewhain.net>
2021-12-09 17:40:38 +0900

    화면 UI 추가

--- Ewhaverse_multi-user/Assets/Scripts/Launcher.cs
+++ Ewhaverse_multi-user/Assets/Scripts/Launcher.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public Text text;
 
     string gameVersion = "1";   //버전
     string currevent;   //이벤트 발생 시 해당 오브젝트 이름 저장할 변수
@@ -21,7 +23,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         //마스터 서버에 접속 중이라면
         if (PhotonNetwork.IsConnected)
         {
-            
+
             PhotonNetwork.JoinRoom(currevent);
 
             //PhotonNetwork.JoinRandomRoom();
@@ -35,14 +37,14 @@ public class Launcher : MonoBehaviourPunCallbacks
     //마스터 서버 연결 성공시 자동 실행
     public override void OnConnectedToMaster()
     {
-        
-        
+        text.text = "서버 연결 성공! 방에 입장하세요!";
     }
 
     //마스터 서버 연결 실패시 자동 실행
     public override void OnDisconnected(DisconnectCause cause)
     {
-        
+        text.text = "연결 중...";
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // 룸에 들어갈 때 호출
@@ -55,6 +57,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     // 룸 입장 실패 시(
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
+        text.text = "방 입장 중...잠시만 기다려 주세요";
         PhotonNetwork.CreateRoom(currevent, new RoomOptions { MaxPlayers = 10 });
     }
 }
